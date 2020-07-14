33ab059a9fc7d1f383eb40227fedfbdd7dfd916a
CodingCCarpenter <Christineassists@aol.com>
2020-07-13 22:04:10 -0500

    create store object from our object constructor

--- store.py
+++ store.py
@@ -4,3 +4,9 @@ class Store:
         # attributes
         self.name = name 
         self.categories = categories
+
+    def __str__ (self):
+        return f'{self.name} has {len (self.categories) } categories''
+
+my_store = Store("Pets Plus", [])
+print(my_store)
